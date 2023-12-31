d982d4f67ce58b503379ca77c144e5c853fd2b44
Tanish Sharma <tanishsharma0209@gmail.com>
2023-12-31 13:51:19 +0530

    new branch

--- src/main/java/com/codeial/codeial/controllers/AuthController.java
+++ src/main/java/com/codeial/codeial/controllers/AuthController.java
@@ -12,38 +12,38 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.codeial.codeial.entity.UserEntity;
 import com.codeial.codeial.service.UserService;
-import com.codeial.codeial.utilities.AuthResponse;
-
+//import com.codeial.codeial.utilities.AuthResponse;
+//
 @RestController
 public class AuthController {
 	
-	@Autowired
-	private UserService userService;
-	
-	@PostMapping("/login")
-	public ResponseEntity<AuthResponse> login(@RequestBody UserEntity userCredentails){
-		List<UserEntity> users = userService.getAllUsers();
-		AuthResponse response;
-		for(UserEntity user : users) {
-			if(user.getEmail().equals(userCredentails.getEmail()) && user.getPassword().equals(userCredentails.getPassword())) {
-				response = new AuthResponse("User logged in successfully", true, user);
-				return new ResponseEntity<>(response, HttpStatus.OK);
-			}
-		}
-		response = new AuthResponse("Invalid credentials", false, null);
-		return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
-	}
-	
-	@PostMapping("/signup")
-	public ResponseEntity<AuthResponse> signup(@RequestBody UserEntity user){
-		UserEntity newUser = new UserEntity(user.getUsername(), user.getEmail(), user.getPassword());// created this for ID
-		AuthResponse response;
-		HttpStatus status = userService.addUser(newUser);	
-		if(status==HttpStatus.OK) {
-			response = new AuthResponse("Signup successfull", true, newUser);
-			return new ResponseEntity<AuthResponse>(response, status);
-		}
-		response = new AuthResponse("Something went wrong", false, null);
-		return new ResponseEntity<AuthResponse>(response, status);
-	}	
+//	@Autowired
+//	private UserService userService;
+//	
+//	@PostMapping("/login")
+//	public ResponseEntity<AuthResponse> login(@RequestBody UserEntity userCredentails){
+//		List<UserEntity> users = userService.getAllUsers();
+//		AuthResponse response;
+//		for(UserEntity user : users) {
+//			if(user.getEmail().equals(userCredentails.getEmail()) && user.getPassword().equals(userCredentails.getPassword())) {
+//				response = new AuthResponse("User logged in successfully", true, user);
+//				return new ResponseEntity<>(response, HttpStatus.OK);
+//			}
+//		}
+//		response = new AuthResponse("Invalid credentials", false, null);
+//		return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+//	}
+//	
+//	@PostMapping("/signup")
+//	public ResponseEntity<AuthResponse> signup(@RequestBody UserEntity user){
+//		UserEntity newUser = new UserEntity(user.getUsername(), user.getEmail(), user.getPassword());// created this for ID
+//		AuthResponse response;
+//		HttpStatus status = userService.addUser(newUser);	
+//		if(status==HttpStatus.OK) {
+//			response = new AuthResponse("Signup successfull", true, newUser);
+//			return new ResponseEntity<AuthResponse>(response, status);
+//		}
+//		response = new AuthResponse("Something went wrong", false, null);
+//		return new ResponseEntity<AuthResponse>(response, status);
+//	}	
 }
--- src/main/java/com/codeial/codeial/entity/UserEntity.java
+++ src/main/java/com/codeial/codeial/entity/UserEntity.java
@@ -5,26 +5,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class UserEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "user_id")
 	private Long user_id;
-	
-	public UserEntity() {// this default constructor is necessary for entityManagerFactory
-		super();
-	}
-
-	public UserEntity(String userName, String email, String password) {
-		super();
-		this.username = userName;
-		this.email = email;
-		this.password = password;
-	}
-	
+		
 	@Column(name = "username")
 	private String username;
 	
@@ -32,38 +25,4 @@ public class UserEntity {
 	private String email;
 	
 	
-	private String password; // encrypt/decrypt later
-
-
-	public String getEmail() {
-		return email;
-	}
-
-	public Long getUser_id() {
-		return user_id;
-	}
-
-	public void setUser_id(Long user_id) {
-		this.user_id = user_id;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
 }
